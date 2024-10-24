3e75e3b65a4c86cbc71dca56a17bd0c32d1203b1
gugoo <gugoo2099@gmail.com>
2024-10-24 14:01:53 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+
+// Main Swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 1,
+    slidesPerView : 'auto', // 한 슬라이드에 보여줄 갯수
+    centeredSlides: true,
+    effect: 'fade',
+    loop : true, // 무한 반복
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+// 헤더푸터 가져오기
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
