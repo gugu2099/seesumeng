5d7a41bab1f40842850acb5605244e5a05528c1b
gugoo <gugoo2099@gmail.com>
2024-10-24 16:41:41 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -22,20 +22,32 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
+
 // 헤더푸터 가져오기
-window.addEventListener('load', function() {
-    var allElements = document.getElementsByTagName('*');
-    Array.prototype.forEach.call(allElements, function(el) {
-        var includePath = el.dataset.includePath;
-        if (includePath) {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    el.outerHTML = this.responseText;
-                }
-            };
-            xhttp.open('GET', includePath, true);
-            xhttp.send();
-        }
+
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
     });
-});
+
+
+
+// 헤더푸터 가져오기
+
+// window.addEventListener('load', function() {
+//     var allElements = document.getElementsByTagName('*');
+//     Array.prototype.forEach.call(allElements, function(el) {
+//         var includePath = el.dataset.includePath;
+//         if (includePath) {
+//             var xhttp = new XMLHttpRequest();
+//             xhttp.onreadystatechange = function () {
+//                 if (this.readyState == 4 && this.status == 200) {
+//                     el.outerHTML = this.responseText;
+//                 }
+//             };
+//             xhttp.open('GET', includePath, true);
+//             xhttp.send();
+//         }
+//     });
+// });
