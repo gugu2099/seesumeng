6f09e4a6a62a52fc15f18bf43b1db969a6e6ccf5
gugoo <gugoo2099@gmail.com>
2024-10-29 14:41:14 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -22,37 +22,37 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
-// Header 불러오기
-fetch('header.html')  // 'header.html' 파일을 불러옴
-    .then(response => response.text())  // 응답을 텍스트로 변환
-    .then(data => {
-        document.getElementById('header').innerHTML = data;  // 'header' div에 삽입
-    })
-    .catch(error => console.log('Error loading header:', error));
+// // Header 불러오기
+// fetch('header.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('header').innerHTML = data;
+//     })
+//     .catch(error => console.log('Error loading header:', error));
 
-// Footer 불러오기
-fetch('footer.html')  // 'footer.html' 파일을 불러옴
-    .then(response => response.text())  // 응답을 텍스트로 변환
-    .then(data => {
-        document.getElementById('footer').innerHTML = data;  // 'footer' div에 삽입
-    })
-    .catch(error => console.log('Error loading footer:', error));
+// // Footer 불러오기
+// fetch('footer.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('footer').innerHTML = data;
+//     })
+//     .catch(error => console.log('Error loading footer:', error));
 
 
-// 헤더푸터 가져오기
-// window.addEventListener('load', function() {
-//     var allElements = document.getElementsByTagName('*');
-//     Array.prototype.forEach.call(allElements, function(el) {
-//         var includePath = el.dataset.includePath;
-//         if (includePath) {
-//             var xhttp = new XMLHttpRequest();
-//             xhttp.onreadystatechange = function () {
-//                 if (this.readyState == 4 && this.status == 200) {
-//                     el.outerHTML = this.responseText;
-//                 }
-//             };
-//             xhttp.open('GET', includePath, true);
-//             xhttp.send();
-//         }
-//     });
-// });
+헤더푸터 가져오기
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
