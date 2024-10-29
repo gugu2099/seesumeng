fb104e3fbc7f37d73200384d6492a0acc1c807df
gugoo <gugoo2099@gmail.com>
2024-10-29 16:14:38 +0900

    first commit

--- international/js/international.js
+++ international/js/international.js
@@ -3,7 +3,7 @@
 
 
 // Header 불러오기
-fetch('../header2.html')  // 상위 폴더로 이동하여 'header.html' 파일 불러옴
+fetch('../header.html')  // 상위 폴더로 이동하여 'header.html' 파일 불러옴
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
@@ -11,7 +11,7 @@ fetch('../header2.html')  // 상위 폴더로 이동하여 'header.html' 파일 
     .catch(error => console.log('Error loading header:', error));
 
 // Footer 불러오기
-fetch('../footer2.html')  // 상위 폴더로 이동하여 'footer.html' 파일 불러옴
+fetch('../footer.html')  // 상위 폴더로 이동하여 'footer.html' 파일 불러옴
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
