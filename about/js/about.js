63dda2f8181e4f4fad799ae75a1acdeb33f26550
gugoo <gugoo2099@gmail.com>
2024-10-29 14:57:21 +0900

    first commit

--- about/js/about.js
+++ about/js/about.js
@@ -3,7 +3,7 @@
 
 
 // Header 불러오기
-fetch('header.html')
+fetch('../header.html')  // 상위 폴더로 이동하여 'header.html' 파일 불러옴
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
@@ -11,7 +11,7 @@ fetch('header.html')
     .catch(error => console.log('Error loading header:', error));
 
 // Footer 불러오기
-fetch('footer.html')
+fetch('../footer.html')  // 상위 폴더로 이동하여 'footer.html' 파일 불러옴
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
