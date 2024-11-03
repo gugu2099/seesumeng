6dcd8053cd3bdb4f9548dcc4a4fa3a510329675e
gugoo <gugoo2099@gmail.com>
2024-11-04 08:27:23 +0900

    first commit

--- bim/js/bim.js
+++ bim/js/bim.js
@@ -2,18 +2,18 @@
 
 
 
-// Header 불러오기
-fetch('../header.html')  // 상위 폴더로 이동하여 'header.html' 파일 불러옴
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    })
-    .catch(error => console.log('Error loading header:', error));
+// // Header 불러오기
+// fetch('../header.html')  // 상위 폴더로 이동하여 'header.html' 파일 불러옴
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('header').innerHTML = data;
+//     })
+//     .catch(error => console.log('Error loading header:', error));
 
-// Footer 불러오기
-fetch('../footer.html')  // 상위 폴더로 이동하여 'footer.html' 파일 불러옴
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer').innerHTML = data;
-    })
-    .catch(error => console.log('Error loading footer:', error));
+// // Footer 불러오기
+// fetch('../footer.html')  // 상위 폴더로 이동하여 'footer.html' 파일 불러옴
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('footer').innerHTML = data;
+//     })
+//     .catch(error => console.log('Error loading footer:', error));
